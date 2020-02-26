6c0ffb29e5a491e65e48c5303841aabd945d078f
v-himanshuttn <himanshu.verma@tothenew.com>
2020-02-26 20:51:17 +0530

    my new gradle

---
+++ FirstGradle1/src/main/javaCode1/Javacode1.java
@@ -0,0 +1,7 @@
+package javaCode1;
+
+public class Javacode1 {
+    public static void main(String[] args) {
+        System.out.println("Java code 1");
+    }
+}
---
+++ FirstGradle1/src/main/javaCode2/Javacode2.java
@@ -0,0 +1,7 @@
+package javaCode2;
+
+public class Javacode2 {
+    public static void main(String[] args) {
+        System.out.println("Java code 2");
+    }
+}
